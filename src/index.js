b3820440c636bcb00072dd3e373552c353af763c
unknown <eduardo.henrique204@gmail.com>
2020-12-12 16:57:30 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const swaggerDocument = require('../swagger.json')
 
 app.use(cors());
 
-uri = process.env.ATLAS_URI || '';
+const uri = process.env.ATLAS_URI||'mongodb://edu:1123581321@cluster0-shard-00-00.ubafb.mongodb.net:27017,cluster0-shard-00-01.ubafb.mongodb.net:27017,cluster0-shard-00-02.ubafb.mongodb.net:27017/test?ssl=true&replicaSet=atlas-fwpg8v-shard-0&authSource=admin&retryWrites=true&w=majority'
+const PORT = process.env.port||'8080'
 
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }).catch(err => console.log(err));
 
@@ -21,6 +22,6 @@ app.use('/apiDocs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(routes);
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
