7c0dfce6c2aacd3210fc208b8a4ddae90763cb33
unknown <eduardo.henrique204@gmail.com>
2020-12-12 16:33:47 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const App = require('./infra/express/app')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express')
 
-var port = process.env.PORT || 8080
+const routes = require('./infra/express/Routes');
 
-App.listen(port)
+const app = express();
+
+const swaggerDocument = require('../swagger.json')
+
+app.use(cors());
+
+var port = process.env.PORT || 8080;
+uri = process.env.ATLAS_URI || '';
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }).catch(err => console.log(err));
+
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/apiDocs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use(routes);
+
+app.listen(port);
